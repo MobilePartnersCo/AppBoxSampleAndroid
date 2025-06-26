7d43f2cb7d3ad779c3b10313b5cbd359fc3e5f1c
최진영 <wlsdud8598@gmail.com>
2025-06-26 11:18:54 +0900

    no message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,6 @@ dependencies {
     // --------------------------------------------------------------
     // implementation 선언
     // --------------------------------------------------------------
-    implementation("com.github.MobilePartnersCo:AppBoxSDKPackage:all-v1.0.28")
+    implementation("com.github.MobilePartnersCo:AppBoxSDKPackage:all-v1.0.29")
     // --------------------------------------------------------------
 }
